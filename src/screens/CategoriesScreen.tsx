640c5daf06baab034f1148af4ae5d8426e046792
Eren AVAR <eren.avar@hotmail.com>
2024-05-17 17:55:56 +0200

    fetch categories from api

--- src/screens/CategoriesScreen.tsx
+++ src/screens/CategoriesScreen.tsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Button, Text, View } from 'react-native'
 import { useNavigation } from '@react-navigation/native'
+
+
+interface ICategories  {
+  categories:string[],
+  pending:boolean,
+  errorMsg:string
+}
+
+// const [categories, setCategories] = useState<ICategories>()
 
 
 
 const CategoriesScreen = () => {
   const navigation:any = useNavigation();
+
+  useEffect(() =>{
+   
+  },[])
+
+  const fetchCategories = async () =>{
+    const categories = fetch('https://fakestoreapi.com/products/categories');
+    const categoriesJson = await (await categories).json();
+    console.log(categoriesJson);
+  }
+
+  fetchCategories();
+
 
   return (
     <View>
