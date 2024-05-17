6407b2e83029d9e04b9dc8155d4977ed67f25840
Eren AVAR <eren.avar@hotmail.com>
2024-05-17 22:07:56 +0200

    list categories with using flatlist without css

--- src/screens/CategoriesScreen.tsx
+++ src/screens/CategoriesScreen.tsx
@@ -1,38 +1,68 @@
-import React, { useEffect } from 'react'
-import { Button, Text, View } from 'react-native'
+import React, { useEffect,useState } from 'react'
+import { Button, FlatList, Text, View } from 'react-native'
 import { useNavigation } from '@react-navigation/native'
 
 
 interface ICategories  {
-  categories:string[],
+  categories:string[] | null,
   pending:boolean,
   errorMsg:string
 }
 
-// const [categories, setCategories] = useState<ICategories>()
+interface Iid  {
+  id:number
+}
+
 
 
 
 const CategoriesScreen = () => {
+  const [categories, setCategories] = useState<ICategories>()
   const navigation:any = useNavigation();
 
   useEffect(() =>{
-   
+  fetchCategories();
+
   },[])
 
   const fetchCategories = async () =>{
-    const categories = await fetch('https://fakestoreapi.com/products/categories');
-    const categoriesJson = await categories.json();
-    console.log(categoriesJson);
+    try {
+      setCategories((prevState) => ({
+        ...prevState,
+        pending:false,
+        errorMsg:""
+      }));
+      const categories = await fetch('https://fakestoreapi.com/products/categories');
+      const categoriesJson = await categories.json();
+
+      setCategories((prevState) =>({
+        ...prevState,
+        categories : categoriesJson,
+        pending:false,
+        errorMsg:""
+   
+      }));
+
+
+    } catch (err) {
+      console.log("Error :", err)
+    }
+    console.log(categories.categories)
   }
 
-  fetchCategories();
+
+
 
 
   return (
     <View>
       <Text>Deneme</Text>
       <Button title='Test' onPress={() => navigation.navigate("Products")} />
+      <FlatList
+        data={categories.categories}
+        renderItem={({item}) => <Text>{item}</Text>}
+        keyExtractor={(item, index) => index.toString()} 
+      />
     </View>
   )
 }
