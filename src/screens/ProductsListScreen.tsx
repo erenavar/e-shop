b1ab1d9dc85a1f8903e82b1610c9d897f4375926
Eren AVAR <eren.avar@hotmail.com>
2024-05-18 20:33:14 +0200

    create interfaces for products

--- src/screens/ProductsListScreen.tsx
+++ src/screens/ProductsListScreen.tsx
@@ -1,8 +1,25 @@
 import { useRoute } from '@react-navigation/native'
-import React from 'react'
+import React, { useState } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 
+interface IProducts {
+  id:number;
+  title:string;
+  price:number;
+  description:string;
+  category:string;
+  image:string;
+  rating:IRating;
+}
+
+interface IRating{
+  rate:number;
+  count:number;
+}
+
 const ProductsListScreen = ({route}) => {
+
+const [products,setProducts] = useState<IProducts>();
 
   return (
     <View>
