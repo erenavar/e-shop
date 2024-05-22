77f56b5b5f5571d43d419af92333c9aa76debb5c
Eren AVAR <eren.avar@hotmail.com>
2024-05-22 18:17:30 +0200

    create activity indicator component

---
+++ src/components/ActivityIndicator.tsx
@@ -0,0 +1,10 @@
+import { StyleSheet } from 'react-native'
+import React from 'react'
+
+export default function ActivityIndicator() {
+  return (
+    <ActivityIndicator/>
+  )
+}
+
+const styles = StyleSheet.create({})
